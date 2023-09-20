6e17c3a0cee29a51de6fc798606406712d97be5b
Galina <galina.mckoy@gmail.com>
2023-09-20 19:29:00 +0200

    implemented counter functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Switching the large product image by clicking on the small thumbnail images
+
 
 let thumbnailImages = document.querySelectorAll('.product__thumb img');
 let productImage = document.querySelector('.product__img');
@@ -9,6 +11,44 @@ thumbnailImages.forEach((thumbnail, index) => {
         productImage.alt = thumbnail.alt;
     });
 });
+
+
+//Counter for a product item and cart
+
+let substructItem = document.querySelector('.counter__substract');
+let addItem = document.querySelector('.counter__add');
+let numberItem = document.querySelector('.counter__number');
+let cartCounter = document.querySelector('.cart__counter');
+
+let counter = 0;
+let price = 125;
+
+//function to update the quantity and total price
+function updateQuantity() {
+    numberItem.textContent = counter;
+    cartCounter.textContent = counter;
+    //Calculate the total price
+    // let totalPrice = price * counter;
+    // console.log(totalPrice);
+}
+// Event listener for the decrement button
+substructItem.addEventListener('click', () => {
+    if (counter > 0) {
+        counter--;
+        updateQuantity();
+    }
+});
+
+// Event listener for the increment button
+addItem.addEventListener('click', () => {
+    counter++;
+    updateQuantity();
+});
+
+// Initial update
+updateQuantity();
+
+
 
 
 
