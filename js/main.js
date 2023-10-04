b566c84a3ea97d3a404d6e6491f5c5d1bfcf7d77
Galina <galina.mckoy@gmail.com>
2023-10-04 11:11:08 +0200

    add toggle on cart icon

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let productImage = document.querySelector('.product__img');
 
 // Add click event listeners to each thumbnail image
 thumbnailImages.forEach((thumbnail, index) => {
-    
+
     thumbnail.addEventListener('click', () => {
         productImage.src = `/images/image-product-${index + 1}.jpg`;
         productImage.alt = thumbnail.alt;
@@ -77,6 +77,16 @@ addToCartButton.addEventListener('click', () => {
     }
     document.querySelector('.counter__number').value = 0;
     numberItem = 0;
+});
+
+
+//Cart rendering
+
+let cartModal = document.querySelector('.cart-modal');
+let cartIcon = document.getElementById('cart');
+
+cartIcon.addEventListener('click', () => {
+    cartModal.classList.toggle('show');
 });
 
 
